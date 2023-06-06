a08c2b647d577c9fe6908aaef6c4b3cbc6cc1803
Brian Koehler <brian.koehler@nic.bc.ca>
2023-06-06 00:19:01 +0000

    removed debuging code; moved client initialiation outside of request function

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import functions_framework
 from google.cloud import dns
 
+# set up the Google Cloud DNS API Client
+client = dns.Client()
+
+# get the last zone
+# (should probably modify this to select a particular zone)
+zones = client.list_zones()
+last_zone = None
+for zone in zones:
+  last_zone = zone
+
 # Register an HTTP function with the Functions Framework
 @functions_framework.http
 def dgl_dns(request):
-  # set up the Google Cloud DNS API Client
-  client = dns.Client()
-
-  # get the last zone
-  # (should probably modify this to select a particular zone)
-  zones = client.list_zones()
-  last_zone = None
-  for zone in zones:
-    last_zone = zone
-
   # identify IP address of requesting client
   target_ip = request.headers.get( 'X-Forwarded-For', '93.184.216.34')
 
   # construct new virtual hostname
   vhost = 'vm' + target_ip.replace( '.', '-')
 
-  # construct FQDN for new virtual host
+  # construct FQDN for virtual host
   fqdn = vhost + '.' + last_zone.dns_name
 
+  # check to see if the RR already exists
+  # if it does, then return immediately
   rrs = last_zone.list_resource_record_sets()
   for rr in rrs:
     if fqdn == rr.name:
-      print( 'Found match' )
       return fqdn
 
-  # create RR for new virtual host
+  # otherwise, create new RR for virtual host
   record_set = last_zone.resource_record_set( fqdn, 'A', 300, [target_ip] )
   changes = last_zone.changes()
   changes.add_record_set(record_set)
